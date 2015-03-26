2e8dc58b9099af499e91185187fbedbda60a6bfb
lpz <lpz8120903@163.com>
2015-03-25 17:54:52 -0700

    modified:   app/views/address/edit.phtml

--- app/cache/_home_lpz_friends_wish2_app_views_index.volt.php
+++ app/cache/_home_lpz_friends_wish2_app_views_index.volt.php
@@ -6,14 +6,14 @@
         <?php echo $this->tag->stylesheetLink('bootstrap/css/bootstrap.css'); ?>
         <?php echo $this->tag->stylesheetLink('bootstrap/css/bootstrap-responsive.css'); ?>
         <?php echo $this->tag->stylesheetLink('css/style.css'); ?>
+        <?php echo $this->tag->javascriptInclude('js/jquery.min.js'); ?>
+        <?php echo $this->tag->javascriptInclude('bootstrap/js/bootstrap.js'); ?>
+        <?php echo $this->tag->javascriptInclude('js/utils.js'); ?>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="description" content="Your invoices">
         <meta name="author" content="Phalcon Team">
     </head>
     <body>
         <?php echo $this->getContent(); ?>
-        <?php echo $this->tag->javascriptInclude('js/jquery.min.js'); ?>
-        <?php echo $this->tag->javascriptInclude('bootstrap/js/bootstrap.js'); ?>
-        <?php echo $this->tag->javascriptInclude('js/utils.js'); ?>
     </body>
 </html>
